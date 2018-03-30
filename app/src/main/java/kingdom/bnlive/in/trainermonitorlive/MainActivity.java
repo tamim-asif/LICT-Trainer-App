0e10bf8ec3e796304627ad9f5481064c3a4887a3
tamim-asif <theqk171@gmail.com>
2018-03-30 13:31:42 +0600

    Splash Screen Added
    Splash Screen Added

--- app/src/main/java/kingdom/bnlive/in/trainermonitorlive/MainActivity.java
+++ app/src/main/java/kingdom/bnlive/in/trainermonitorlive/MainActivity.java
@@ -3,14 +3,53 @@ package kingdom.bnlive.in.trainermonitorlive;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView;
+    TextView dev1, dev2, copyright;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent=new Intent(MainActivity.this,RegistrationLogin.class);
-        startActivity(intent);
+
+        imageView = findViewById(R.id.imageView2);
+        dev1 = findViewById(R.id.dev1);
+        dev2 = findViewById(R.id.dev2);
+        copyright = findViewById(R.id.copyright);
+
+        dev1.setVisibility(View.GONE);
+        dev2.setVisibility(View.GONE);
+        copyright.setVisibility(View.GONE);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.slide_in_left);
+        animation.setDuration(2500);
+        imageView.startAnimation(animation);
+
+        dev1.setVisibility(View.VISIBLE);
+        dev2.setVisibility(View.VISIBLE);
+        copyright.setVisibility(View.VISIBLE);
+
+//        imageView.animate().scaleX(20).scaleY(20).rotationX(360).rotationY(360).setDuration(2500);
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep( 3000 );
+                    Intent intent=new Intent(MainActivity.this,RegistrationLogin.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+        myThread.start();
+
     }
 }
